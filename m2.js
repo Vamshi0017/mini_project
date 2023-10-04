e63b7dc9a0b631dc5e25b88781754237555a1cde
PARUPELLY VAMSHI KRISHNA <102134257+Vamshi0017@users.noreply.github.com>
2023-10-04 17:29:03 +0530

    Update m2.js

--- m2.js
+++ m2.js
@@ -4,24 +4,29 @@ let a = document.getElementById('n');
     let d = document.getElementById('cp');
     let e = document.getElementById('m');
     let f = document.getElementById('h');
+    let k=0;
 function myfunc(){
     let z = ( a || b || c || d);
     if(z.value == "" || e.value == 0)
     {
-        window.alert("none");
+        window.alert("please give the values");
+        //k=1;
     }
     else if(c.value != d.value){
-        window.alert("no");
+        window.alert("please give the same password");
+        //k=1
     }
     else{
         window.open("m1.html");
+        //k=1
     }
 }
+
 const form = document.getElementById("form");
 form.addEventListener('submit',function(e){
-    e.preventDefault();
+e.preventDefault();
 localStorage.setItem("username",b.value);
 localStorage.setItem("password",c.value);
 localStorage.setItem("hint",f.value);
-window.location.href = "m1.js";
-})
+}
+)
